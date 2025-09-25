8d7cef17378b3306c40fc0e519742a9c77cded2d
Markus Varckin <varckin@protonmail.com>
2025-09-25 14:23:05 +0300

    fix settings clamav

--- clamav_antivirus/settings.py
+++ clamav_antivirus/settings.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from os import getenv
 
 
 class ClamAVSettings(BaseModel):
-    clamav_host: str = getenv('CLAMAV_HOST','127.0.0.1')
-    clamav_port: int = int(getenv('CLAMAV_PORT','3310'))
+    clamav_host: str = Field(default_factory=lambda: getenv('CLAMAV_HOST', '127.0.0.1'))
+    clamav_port: int = Field(default_factory=lambda: int(getenv('CLAMAV_PORT', '3310')))
     max_file_size_mb: int = 50
 
 
